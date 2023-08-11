15954c2482e4b8071752345735d1aacdb90d4510
mphstar <bintang.simdig20@gmail.com>
2023-08-11 11:23:40 +0700

    Add project

---
+++ GestureWithImage.py
@@ -0,0 +1,255 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import base64
+import csv
+import copy
+import argparse
+import itertools
+from collections import deque
+
+import cv2 as cv
+import mediapipe as mp
+import numpy as np
+
+from model import KeyPointClassifier
+
+# API
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+from flask_cors import CORS, cross_origin
+import eventlet
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--device", type=int, default=0)
+    parser.add_argument("--width", help="cap width", type=int, default=960)
+    parser.add_argument("--height", help="cap height", type=int, default=540)
+
+    parser.add_argument("--use_static_image_mode", action="store_true")
+    parser.add_argument(
+        "--min_detection_confidence",
+        help="min_detection_confidence",
+        type=float,
+        default=0.7,
+    )
+    parser.add_argument(
+        "--min_tracking_confidence",
+        help="min_tracking_confidence",
+        type=int,
+        default=0.5,
+    )
+
+    args = parser.parse_args()
+
+    return args
+
+
+def GestureDetect(
+    images, hands, point_history, keypoint_classifier, keypoint_classifier_labels
+):
+    image = cv.flip(images, 1)  # Mirror display
+    debug_image = copy.deepcopy(image)
+
+    # Detection implementation #############################################################
+    image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+
+    image.flags.writeable = False
+    results = hands.process(image)
+    image.flags.writeable = True
+
+    #  ####################################################################
+    if results.multi_hand_landmarks is not None:
+        for hand_landmarks, handedness in zip(
+            results.multi_hand_landmarks, results.multi_handedness
+        ):
+            # Landmark calculation
+            landmark_list = calc_landmark_list(debug_image, hand_landmarks)
+
+            # Conversion to relative coordinates / normalized coordinates
+            pre_processed_landmark_list = pre_process_landmark(landmark_list)
+
+            # Hand sign classification
+            try:
+                hand_sign_id = keypoint_classifier(pre_processed_landmark_list)
+                if hand_sign_id == 2:  # Point gesture
+                    point_history.append(landmark_list[8])
+                else:
+                    point_history.append([0, 0])
+
+                result = {
+                    "hand": handedness.classification[0].label,
+                    "result": keypoint_classifier_labels[hand_sign_id],
+                }
+                return result
+            except:
+                return "null"
+    else:
+        return "null"
+
+
+def calc_landmark_list(image, landmarks):
+    image_width, image_height = image.shape[1], image.shape[0]
+
+    landmark_point = []
+
+    # Keypoint
+    for _, landmark in enumerate(landmarks.landmark):
+        landmark_x = min(int(landmark.x * image_width), image_width - 1)
+        landmark_y = min(int(landmark.y * image_height), image_height - 1)
+        # landmark_z = landmark.z
+
+        landmark_point.append([landmark_x, landmark_y])
+
+    return landmark_point
+
+
+def pre_process_landmark(landmark_list):
+    temp_landmark_list = copy.deepcopy(landmark_list)
+
+    # Convert to relative coordinates
+    base_x, base_y = 0, 0
+    for index, landmark_point in enumerate(temp_landmark_list):
+        if index == 0:
+            base_x, base_y = landmark_point[0], landmark_point[1]
+
+        temp_landmark_list[index][0] = temp_landmark_list[index][0] - base_x
+        temp_landmark_list[index][1] = temp_landmark_list[index][1] - base_y
+
+    # Convert to a one-dimensional list
+    temp_landmark_list = list(itertools.chain.from_iterable(temp_landmark_list))
+
+    # Normalization
+    max_value = max(list(map(abs, temp_landmark_list)))
+
+    def normalize_(n):
+        return n / max_value
+
+    temp_landmark_list = list(map(normalize_, temp_landmark_list))
+
+    return temp_landmark_list
+
+
+def pre_process_point_history(image, point_history):
+    image_width, image_height = image.shape[1], image.shape[0]
+
+    temp_point_history = copy.deepcopy(point_history)
+
+    # Convert to relative coordinates
+    base_x, base_y = 0, 0
+    for index, point in enumerate(temp_point_history):
+        if index == 0:
+            base_x, base_y = point[0], point[1]
+
+        temp_point_history[index][0] = (
+            temp_point_history[index][0] - base_x
+        ) / image_width
+        temp_point_history[index][1] = (
+            temp_point_history[index][1] - base_y
+        ) / image_height
+
+    # Convert to a one-dimensional list
+    temp_point_history = list(itertools.chain.from_iterable(temp_point_history))
+
+    return temp_point_history
+
+
+def base64_to_image(base64_string):
+    # Extract the base64 encoded binary data from the input string
+    base64_data = base64_string.split(",")[1]
+    # Decode the base64 data to bytes
+    image_bytes = base64.b64decode(base64_data)
+    # Convert the bytes to numpy array
+    image_array = np.frombuffer(image_bytes, dtype=np.uint8)
+    # Decode the numpy array as an image using OpenCV
+    image = cv.imdecode(image_array, cv.IMREAD_COLOR)
+    return image
+
+# Custom InputStreamHandler
+# Custom InputStreamHandler
+class MyInputStreamHandler:
+    def __init__(self):
+        self.data_buffer = []
+
+    def fileno(self):
+        return self
+
+    def readinto(self, data):
+        if self.data_buffer:
+            data[:len(self.data_buffer)] = self.data_buffer
+            del self.data_buffer[:]
+            return len(data)
+        return None
+
+    def put(self, data):
+        self.data_buffer.extend(data)
+
+
+
+# SETUP PROGRAM!!
+app = Flask(__name__)
+CORS(app)
+app.config["SECRET_KEY"] = "secret!"
+socketio = SocketIO(app, cors_allowed_origins="*")
+input_handler = MyInputStreamHandler()
+
+# Argument parsing #################################################################
+args = get_args()
+
+use_static_image_mode = args.use_static_image_mode
+min_detection_confidence = args.min_detection_confidence
+min_tracking_confidence = args.min_tracking_confidence
+
+# Model load #############################################################
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(
+    static_image_mode=use_static_image_mode,
+    max_num_hands=1,
+    min_detection_confidence=min_detection_confidence,
+    min_tracking_confidence=min_tracking_confidence,
+)
+
+keypoint_classifier = KeyPointClassifier()
+
+# Read labels ###########################################################
+with open(
+    "model/keypoint_classifier/keypoint_classifier_label.csv", encoding="utf-8-sig"
+) as f:
+    keypoint_classifier_labels = csv.reader(f)
+    keypoint_classifier_labels = [row[0] for row in keypoint_classifier_labels]
+
+# Coordinate history #################################################################
+history_length = 16
+point_history = deque(maxlen=history_length)
+
+
+@socketio.on("tesconnect")
+def connect(text):
+    print(text)
+    # emit("processed_image", {"result": "success"})
+
+# Function socket
+@socketio.on("image")
+def receive_image(image):
+    # Decode the base64-encoded image data
+    image = base64_to_image(image)
+
+    input_handler.put(image)
+
+    result = GestureDetect(
+        images=image,
+        hands=hands,
+        point_history=point_history,
+        keypoint_classifier=keypoint_classifier,
+        keypoint_classifier_labels=keypoint_classifier_labels,
+    )
+
+    print(result)
+
+    # end gesture recognition
+    emit("processed_image", {"result": result})
+
+
+if __name__ == "__main__":
+    socketio.run(app, debug=True, port=5000)
